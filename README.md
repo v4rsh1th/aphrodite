43d1eb86e2259de285b669f7a83e99896cde9ded
v4rsh1th <v4rsh1th@gmail.com>
2022-05-15 09:06:20 +0530

    Entities setup.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Aphrodite
+
+Social news aggregation, web content rating, and discussion. Registered users upload content to the site, such as links, text entries, images, and videos, which are then rated up or down by other users.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hey');
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+
+  const post = orm.em.create(Post, { title: "this is the first post." });
+  await orm.em.persistAndFlush(post);
+  await orm.em.nativeInsert(Post, { title: "this is the second post." });
+};
+
+main().catch(err => {
+  console.log(err);
+});
